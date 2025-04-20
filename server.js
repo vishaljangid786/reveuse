c686b6811aa2ef614b100ef4612ac92643df4d72
vishaljangid786 <vishaljangid80550786@gmail.com>
2025-04-20 09:08:14 +0530

    first commit

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const app = express();
 connectDB();
 
 const corsOptions = {
-  origin: "http://localhost:5173",
+  origin: "https://the-reveuse.vercel.app/",
   credentials: true,
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization", "X-Requested-With"],
