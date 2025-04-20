b1df15fb61e403463fff7368d7f777734f6364bb
vishaljangid786 <vishaljangid80550786@gmail.com>
2025-04-20 10:34:22 +0530

    commited

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ const dotenv = require("dotenv");
 const cors = require("cors");
 const connectDB = require("./config/db");
 const blogRoutes = require("./routes/BlogRoutes");
+const bodyParser = require('body-parser')
+const jwt = require('jsonwebtoken')
 
 dotenv.config();
 
@@ -20,10 +22,33 @@ const corsOptions = {
   optionsSuccessStatus: 204,
 };
 
-app.use(cors(corsOptions));
+app.use(cors());
 app.use(express.json());
+app.use(bodyParser.json());
 
 app.use("/api/blogs", blogRoutes);
+
+app.post("/api/users/login", (req, res) => {
+  const { email, password } = req.body;
+
+  // Check if credentials match the static user
+  if (
+    email === process.env.STATIC_EMAIL &&
+    password === process.env.STATIC_PASSWORD) {
+    // Generate token
+    const token = jwt.sign({ email }, process.env.JWT_SECRET, { expiresIn: "1h" });
+
+    return res.json({
+      message: "Login successful",
+      token,
+    });
+  }
+
+  return res.status(401).json({
+    message: "Invalid email or password",
+  });
+});
+
 
 const PORT = process.env.PORT || 5000;
 
