9ea6b1bfadec45a69ece12d5ee813c1fc67b6373
vishaljangid786 <vishaljangid80550786@gmail.com>
2025-04-21 08:18:21 +0530

    com

--- config/cloudinary.js
+++ config/cloudinary.js
@@ -1,14 +1,14 @@
 const cloudinary = require("cloudinary").v2;
 const multer = require("multer");
 
-const CLOUDINARY_CLOUD_NAME = "djvxynk2f";
-const CLOUDINARY_API_KEY = "917629743935231";
-const CLOUDINARY_API_SECRET = "6-nMEpJVIa1_RUj_aeiR6hsGFw8";
+// const CLOUDINARY_CLOUD_NAME = "djvxynk2f";
+// const CLOUDINARY_API_KEY = "917629743935231";
+// const CLOUDINARY_API_SECRET = "6-nMEpJVIa1_RUj_aeiR6hsGFw8";
 
 cloudinary.config({
-  cloud_name: CLOUDINARY_CLOUD_NAME,
-  api_key: CLOUDINARY_API_KEY,
-  api_secret: CLOUDINARY_API_SECRET,
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 
 // Configure multer memory storage
